93c798649d047eb7a32e9b464165d8f2764a5cf1
boonmojae <pminny99@naver.com>
2025-06-04 18:29:57 +0900

    0604 commit

--- mvc/config/db.js
+++ mvc/config/db.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2/promise');
+//등록만 하면 사용할 수 있어서 변수처리 안함
+require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   port: 3306,
-  password: "1111",
-  database: "test_shop",
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = pool;
+
